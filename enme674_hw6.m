9ec0c958e7e3aaa64300a9ec9cb7fdece6966400
jay-oliver <jayoliveryen@gmail.com>
2024-10-16 16:28:04 -0400

    submitted once

--- enme674_hw6.m
+++ enme674_hw6.m
@@ -7,7 +7,6 @@ F = 2000 %N
 N1 = (l-s)/l;
 N2 = s/l;
 N = [N1 N2]
-u = [u1, u2, u3, u4];
 % Find B
 B = diff(N, s)
 %Find K
@@ -36,5 +35,15 @@ sigma1 = E*B*[0; u(1)]
 sigma2 = E*B*[u(1); u(2)]
 sigma3 = E*B*[u(2); u(3)]
 
-stress1 = simplify(sigma1)
-st
+stress1 = simplify(sigma1);
+stress2 = simplify(sigma2);
+stress3 = simplify(sigma3);
+
+x1 = 0:1/50:1/3;
+x2 = 1/3:1/50:2/3;
+x3 = 2/3:1/50:1;
+RR_u1 = x1*(a*l^2)/(27*E*A);
+RR_u2 = (a*l^3)/(81*E*A) + x2*((7*a*l^2)/(27*E*A) + (3*F)/(2*A*E));
+RR_u3 = (8*a*l^3)/(81*E*A) + (F*l)/(E*A) + x3*((3*F)/(a*E) + (19*a*l^2)/(27*E*A));
+figure
+plot(x1, RR_u1, x2, RR_u2, x3, RR_u3)
